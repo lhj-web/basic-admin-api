e76195e3ce6a242c23b9277efbd6f675af1d3a1a
lhj <1871731528@qq.com>
2022-01-24 11:45:43 +0800

    feat: initialize project

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    logger: new Logger(),
+  });
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3000);
+}
+bootstrap();

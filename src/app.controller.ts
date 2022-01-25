4dcc6ccb55feb3c123efb0da1f3475ab45be03c2
lhj <1871731528@qq.com>
2022-01-25 22:14:57 +0800

    feat: add the app global config

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import * as APP_CONFIG from '@/app.config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  root() {
+    return APP_CONFIG.PROJECT;
   }
 }

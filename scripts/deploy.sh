069e75844dc056c1a3908b69f1454ee16ab5605b
lhj <1871731528@qq.com>
2022-02-24 16:02:32 +0800

    workflow: update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,7 +10,7 @@ echo "[deploy] start deployment..."
 echo "[deploy] fetching..."
 echo "[deploy] path:" $(pwd)
 echo "[deploy] pulling source code..."
-git fetch --all && git reset --hard origin/main && git pull
+git pull
 git checkout main
 
 echo "[deploy] stop service..."
